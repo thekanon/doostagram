3deea67172e8f41a5e056d6860a1ac5323262dec
Doo Lee <dlengjs12@naver.com>
2020-09-01 23:02:27 +0900

    seeFeed 기능 추가 bbc는 테스트용 server.js도 동일함

--- src/server.js
+++ src/server.js
@@ -16,7 +16,7 @@ import { isAuthenticated } from "./middlewares";
 
 //Util(Mail) import
 // import {sendSecretMail} from "./utils"
-// sendSecretMail("dlengjs123@gmail.com","Kanon16Z@Z"); 당장 메일 보낼게 아니므로 일단 제거
+// sendSecretMail("dlengjs123@gmail.com","aaaaaaa@Z"); 당장 메일 보낼게 아니므로 일단 제거
 
 
 //환경변수를 .env에서 읽어오도록 함
@@ -36,3 +36,51 @@ server.express.use(authenticateJwt);
 server.start({port: PORT},() => 
     console.log(`Server running on http://localhost:${PORT}`)
 );
+// express 이용하여 변수 require 변수 선언
+var express = require('express');
+// 미들웨어 변수 선언
+var app = express();
+
+// 클라이언트 정보 추가
+var client_id = 'n3RO1LZqp6aV3zGYnzha';
+var client_secret = 'rGLmrR9FZL';
+
+//번역할 문장 가져옴(json으로 가져오면 좋을듯)
+var query = "I want more. Choose a prettier dress.";
+
+//translate 
+app.get('/translate', function (req, res) {
+
+    const { spawn } = require('child_process');
+    const pyProg = spawn('python', ['./api/Python/bbc.py']);
+    var aaa;
+    pyProg.stdout.on('data', function(data) {
+
+        console.log(data.toString());
+        res.write(data);
+        res.end('end');
+
+        aaa = data
+    });
+    // console.log(pyProg)
+//    var api_url = 'https://openapi.naver.com/v1/papago/n2mt';
+//    var request = require('request');
+//    var options = {
+//        url: api_url,
+//        form: {'source':'en', 'target':'ko', 'text':query},
+//        headers: {'X-Naver-Client-Id':client_id, 'X-Naver-Client-Secret': client_secret}
+//     };
+//    request.post(options, function (error, response, body) {
+//      if (!error && response.statusCode == 200) {
+//        res.writeHead(200, {'Content-Type': 'text/json;charset=utf-8'});
+//        res.end(body);
+//      } else {
+//        res.status(response.statusCode).end();
+//        console.log('error = ' + response.statusCode);
+//      }
+//    });
+ });
+ app.listen(3000, function () {
+   console.log('http://127.0.0.1:3000/translate app listening on port 3000!');
+ });
+

f01e4f196ebb8b6059172b005158824d7db3b747
Doo Lee <dlengjs12@naver.com>
2020-09-03 23:47:44 +0900

    오류 테스트

--- src/api/User/requestSecret/requestSecret.js
+++ src/api/User/requestSecret/requestSecret.js
@@ -1,22 +1,18 @@
+import { generateSecret, sendSecretMail } from "../../../utils";
 import { prisma } from "../../../../generated/prisma-client";
-import { generateSecret, sendSecretMail} from "../../../utils";
 
 export default {
-    Mutation: {
-        requestSecret: async(_, args,{request})=>{
-            console.log(request)
-            const { email } = args;
-            const loginSecret = generateSecret();
-            console.log(loginSecret)
-            try {
-                throw Error();
-                await sendSecretMail(email, loginSecret);
-                await prisma.updateUser({data:{loginSecret}, where:{email} });
-                return true
-            }catch(e) {
-                console.log(e);
-                return false
-            }
-        }
+  Mutation: {
+    requestSecret: async (_, args) => {
+      const { email } = args;
+      const loginSecret = generateSecret();
+      try {
+        await sendSecretMail(email, loginSecret);
+        await prisma.updateUser({ data: { loginSecret }, where: { email } });
+        return true;
+      } catch {
+        return false;
+      }
     }
-}
+  }
+};

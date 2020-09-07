a70ce1f31db5f7d94bc5bda174d70c8cd326f1cf
Doo Lee <dlengjs12@naver.com>
2020-09-07 22:49:48 +0900

    backend end end

--- src/api/User/seeUser/seeUser.js
+++ src/api/User/seeUser/seeUser.js
@@ -3,14 +3,7 @@ export default {
     Query: {
         seeUser: async(_, args) => {
             const {id} = args;
-            
-            // return prisma.user({id})
-            const user = await prisma.user({id});
-            const posts = await prisma.user({id}).posts();
-            return {
-                user,
-                posts
-            }
+            return prisma.user({ id }); 
         }
     }
 }
